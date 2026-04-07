e4c1ae605c6d1fa8fe850e6be68e494a0055bed9
Jean <srjean21@gmail.com>
2026-04-07 18:14:51 -0400

    feat: fixed buttons text alignment overall

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -8,6 +8,13 @@ class AppTheme {
   // Base theme configuration (used by both light and dark themes)
   static FluentThemeData _extendBaseTheme(FluentThemeData t) {
     return t.copyWith(
+      // fix fluent btn text alignment
+      buttonTheme: ButtonThemeData(
+        defaultButtonStyle: BtnStyles.fixedTextAlignmentStyle,
+        filledButtonStyle: BtnStyles.fixedTextAlignmentStyle,
+        outlinedButtonStyle: BtnStyles.fixedTextAlignmentStyle,
+        hyperlinkButtonStyle: BtnStyles.fixedTextAlignmentStyle,
+      ),
       navigationPaneTheme: NavigationPaneThemeData(
         // vertical is asymmetric to better align with leading icon
         labelPadding: labelIconAlignPadding,
@@ -26,4 +33,13 @@ class AppTheme {
 
 extension AppThemeBuildContext on BuildContext {
   FluentThemeData get theme => FluentTheme.of(this);
+}
+
+class BtnStyles {
+  BtnStyles._();
+
+  /// For some reason, Fluent text doesn't properly align with leading icon or padding in general. This is a workaround to fix that.
+  static const ButtonStyle fixedTextAlignmentStyle = ButtonStyle(
+    textStyle: WidgetStatePropertyAll(TextStyle(height: 0)),
+  );
 }
