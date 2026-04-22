c3edbc2650e702fa2ad9b06e8a1b427aca510da9
Jean <srjean21@gmail.com>
2026-04-22 15:29:06 -0400

    refactor: moved some widgets to main screen file

--- lib/users/users_screen.dart
+++ lib/users/users_screen.dart
@@ -1,6 +1,14 @@
 import 'package:disco/disco.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_query/flutter_query.dart';
+import 'package:servicable_stock/core/utils/guard_widget.dart';
+import 'package:servicable_stock/shared/widgets/table_title.dart';
+import 'package:servicable_stock/users/users_constants.dart';
+import 'package:servicable_stock/users/users_types.dart';
 import 'package:servicable_stock/users/view_model/users_vm.dart';
+import 'package:servicable_stock/users/widgets/change_role_btn.dart';
+import 'package:servicable_stock/users/widgets/delete_btn.dart';
 import 'package:servicable_stock/users/widgets/table.dart';
 
 class UsersScreen extends StatelessWidget {
@@ -8,9 +16,45 @@ class UsersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      providers: [UsersVm.instance],
-      child: const UsersTable(),
+    return ProviderScope(providers: [UsersVm.instance], child: _UsersScreen());
+  }
+}
+
+class _UsersScreen extends HookWidget {
+  const _UsersScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = UsersVm.instance.of(context);
+
+    final UsersQuery query = useQuery(
+      kUserTableQueryKey,
+      (context) => vm.getUsers(),
+    );
+
+    return ScaffoldPage(
+      header: PageHeader(
+        title: Row(
+          mainAxisAlignment: .spaceBetween,
+          children: [
+            TableTitle(query: query, text: 'Lista de usuarios'),
+
+            const Guard(
+              role: .admin,
+              child: Row(
+                mainAxisAlignment: .end,
+                spacing: 8,
+                children: [DeleteBtn(), ChangeRoleBtn()],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      content: Padding(
+        padding: const .only(bottom: 20, left: 20, right: 20),
+        child: UsersTable(query),
+      ),
     );
   }
 }
--- lib/users/widgets/table.dart
+++ lib/users/widgets/table.dart
@@ -1,6 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_query/flutter_query.dart';
 import 'package:servicable_stock/auth/auth_state.dart';
 import 'package:servicable_stock/core/utils/fn.dart';
 import 'package:servicable_stock/core/utils/table_utils.dart';
@@ -11,125 +10,108 @@ import 'package:servicable_stock/users/users_types.dart';
 import 'package:servicable_stock/users/view_model/users_vm.dart';
 import 'package:servicable_stock/shared/widgets/table_fetch_error.dart';
 import 'package:servicable_stock/users/widgets/grid_card_wrapper.dart';
-import 'package:servicable_stock/users/widgets/table_header.dart';
 import 'package:servicable_stock/users/widgets/watch_shift_key.dart';
 import 'package:trina_grid/trina_grid.dart';
 
 class UsersTable extends HookWidget {
-  const UsersTable({super.key});
+  final UsersQuery query;
+  const UsersTable(this.query, {super.key});
 
   @override
   Widget build(BuildContext context) {
     final vm = UsersVm.instance.of(context);
-    final getUsers = vm.getUsers;
     final selfId = AuthState.instance.of(context).user?.id;
 
-    final UsersQuery query = useQuery(
-      kUserTableQueryKey,
-      (context) => getUsers(),
-    );
+    if (query.isError) {
+      return TableFetchError(query, 'Error al obtener los usuarios');
+    }
 
-    return ScaffoldPage(
-      header: Header(query),
+    final gridConfig = getTrinaBaseConfig(context);
 
-      content: Padding(
-        padding: const .only(bottom: 20, left: 20, right: 20),
-        child: Builder(
-          builder: (context) {
-            if (query.isError) {
-              return TableFetchError(query, 'Error al obtener los usuarios');
+    if (query.isLoading) {
+      return GridCardWrapper(
+        tableLoader(
+          query,
+          config: gridConfig,
+          columns: getColumns(
+            context: context,
+            listLength: 0,
+            selfId: selfId!,
+            cellsPadding: gridConfig.style.defaultCellPadding,
+          ),
+        ),
+      );
+    }
+
+    final usersList = query.data!;
+
+    return WatchShiftKey(
+      onShiftPressed: (pressed) => vm.shiftPressed = pressed,
+      child: GridCardWrapper(
+        TrinaGrid(
+          noRowsWidget: NoRows('usuarios'),
+
+          columns: getColumns(
+            context: context,
+            listLength: usersList.length,
+            selfId: selfId!,
+            cellsPadding: gridConfig.style.defaultCellPadding,
+          ),
+
+          configuration: gridConfig,
+
+          onLoaded: (event) => vm.stateManager = event.stateManager,
+
+          onBeforeActiveCellChange: (event) => false,
+
+          onRowChecked: (event) {
+            final m = vm.stateManager!;
+
+            if (event.isChecked == true) {
+              final start = vm.lastSelectedIndex;
+              final end = event.rowIdx!;
+              final isRange =
+                  vm.lastSelectedIndex > -1 && (start - end).abs() > 1;
+
+              if (!isRange) {
+                vm.lastSelectedIndex = event.rowIdx!;
+              } else {
+                final isForwards = start > end;
+
+                for (
+                  int i = start;
+                  isForwards ? i > end : i < end;
+                  isForwards ? i-- : i++
+                ) {
+                  final row = m.refRows[i];
+
+                  if (i != event.rowIdx && row.$id! != selfId) {
+                    row.setChecked(true);
+                  }
+                }
+
+                vm.lastSelectedIndex = -1;
+                m.notifyListeners();
+              }
+            } else {
+              vm.lastSelectedIndex = -1;
             }
 
-            final gridConfig = getTrinaBaseConfig(context);
+            final bool noRowsChecked = m.checkedRows.isEmpty;
 
-            if (query.isLoading) {
-              return GridCardWrapper(
-                tableLoader(
-                  query,
-                  config: gridConfig,
-                  columns: getColumns(
-                    context: context,
-                    listLength: 0,
-                    selfId: selfId!,
-                    cellsPadding: gridConfig.style.defaultCellPadding,
-                  ),
-                ),
-              );
-            }
+            final allSelected =
+                m.checkedRows.length ==
+                // subtract 1 so it doesn't include current user row, which is disabled
+                m.rows.length - 1;
 
-            final usersList = query.data!;
-
-            return WatchShiftKey(
-              onShiftPressed: (pressed) => vm.shiftPressed = pressed,
-              child: GridCardWrapper(
-                TrinaGrid(
-                  noRowsWidget: NoRows('usuarios'),
-
-                  columns: getColumns(
-                    context: context,
-                    listLength: usersList.length,
-                    selfId: selfId!,
-                    cellsPadding: gridConfig.style.defaultCellPadding,
-                  ),
-
-                  configuration: gridConfig,
-
-                  onLoaded: (event) => vm.stateManager = event.stateManager,
-
-                  onBeforeActiveCellChange: (event) => false,
-
-                  onRowChecked: (event) {
-                    final m = vm.stateManager!;
-
-                    if (event.isChecked == true) {
-                      final start = vm.lastSelectedIndex;
-                      final end = event.rowIdx!;
-                      final isRange =
-                          vm.lastSelectedIndex > -1 && (start - end).abs() > 1;
-
-                      if (!isRange) {
-                        vm.lastSelectedIndex = event.rowIdx!;
-                      } else {
-                        final isForwards = start > end;
-
-                        for (
-                          int i = start;
-                          isForwards ? i > end : i < end;
-                          isForwards ? i-- : i++
-                        ) {
-                          final row = m.refRows[i];
-
-                          if (i != event.rowIdx && row.$id! != selfId) {
-                            row.setChecked(true);
-                          }
-                        }
-
-                        vm.lastSelectedIndex = -1;
-                        m.notifyListeners();
-                      }
-                    } else {
-                      vm.lastSelectedIndex = -1;
-                    }
-
-                    final bool noRowsChecked = m.checkedRows.isEmpty;
-
-                    final allSelected =
-                        m.checkedRows.length ==
-                        // subtract 1 so it doesn't include current user row, which is disabled
-                        m.rows.length - 1;
-
-                    vm.selectAllCheckedValue.value = allSelected
-                        ? true
-                        : noRowsChecked
-                        ? false
-                        : null;
-                  },
-
-                  rows: vm.getRows(context, usersList),
-                ),
-              ),
-            );
+            vm.selectAllCheckedValue.value = allSelected
+                ? true
+                : noRowsChecked
+                ? false
+                : null;
           },
+
+          rows: vm.getRows(context, usersList),
         ),
       ),
     );
