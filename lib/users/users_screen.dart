7e73bf5ee9088720b470ef3cd36afb74d94b32ca
Jean <srjean21@gmail.com>
2026-04-22 14:02:00 -0400

    refactor: made this widget stateless

--- lib/users/users_screen.dart
+++ lib/users/users_screen.dart
@@ -3,14 +3,9 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:servicable_stock/users/view_model/users_vm.dart';
 import 'package:servicable_stock/users/widgets/table.dart';
 
-class UsersScreen extends StatefulWidget {
+class UsersScreen extends StatelessWidget {
   const UsersScreen({super.key});
 
-  @override
-  State<UsersScreen> createState() => _UsersScreenState();
-}
-
-class _UsersScreenState extends State<UsersScreen> {
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
