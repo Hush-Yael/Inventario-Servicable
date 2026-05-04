d84da64a188f6ce352dad03fb300300cf376b634
Jean <srjean21@gmail.com>
2026-05-04 17:38:41 -0400

    feat: added size field

--- lib/shared/widgets/submit_btn_ring.dart
+++ lib/shared/widgets/submit_btn_ring.dart
@@ -2,13 +2,14 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:servicable_stock/core/theme/theme.dart';
 
 class SubmitBtnRing extends StatelessWidget {
-  const SubmitBtnRing({super.key});
+  final double size;
+  const SubmitBtnRing({super.key, this.size = 16});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 16,
-      width: 16,
+      height: size,
+      width: size,
       child: ProgressRing(
         strokeWidth: 3.5,
         backgroundColor: context.theme.accentColor.withAlpha(90),
