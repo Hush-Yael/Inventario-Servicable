6809e6c96d7df17df01d8faa81558977a3ead26a
Jean <srjean21@gmail.com>
2026-05-07 13:04:24 -0400

    feat: added a class that contains common utilities for forms and some functions to better work with async errors

---
+++ lib/shared/form_with_async_validation.dart
@@ -0,0 +1,26 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_solidart/flutter_solidart.dart';
+
+class FormWithAsyncValidation {
+  final formKey = GlobalKey<FormBuilderState>();
+  final isSubmitting = Signal(false);
+
+  bool get enabled => !isSubmitting.value;
+  bool get invalid => formKey.currentState!.validate() != true;
+
+  /// To show async errors, fields are manually invalidated. Those errors are then shown using field.errorText
+  String? fieldSyncAndAsyncValidation<T extends FormFieldState<String?>>(
+    T field, {
+    required String? Function(String?) validator,
+  }) {
+    final error = validator(field.value) ?? field.errorText;
+
+    return error;
+  }
+
+  void changeAndClearAsyncError<T>(T value, FormFieldState<T> field) {
+    if (field.errorText != null) field.validate();
+    field.didChange(value);
+  }
+}
